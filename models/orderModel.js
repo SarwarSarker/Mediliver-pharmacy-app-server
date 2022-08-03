a02fe3fc9401153f5eecc4db60b219ce32df0beb
SarwarSarker <sarwarsarker00@gmail.com>
2022-08-03 06:13:18 +0600

    model updated

--- models/orderModel.js
+++ models/orderModel.js
@@ -15,7 +15,7 @@ const orderSchema = new mongoose.Schema(
         type: String,
         required: true,
       },
-      phoneNo: {
+      phone: {
         type: Number,
         required: true,
       },
